c3ce9327cae22b92bcbc3b7fccc7e41b1cee08d4
eric <ericljx2020@gmail.com>
2023-07-06 22:23:11 +0800

    55.40

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -119,6 +119,63 @@ class GameMap extends AcGameObject{
     }
 
     update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.fillStyle = "rgba(0,0,0,0.1)";
+        this.ctx.fillRect(0,0,this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+}
+class Player extends AcGameObject{
+    constructor(playground, x, y, radius, color, speed, is_me){
+        super();
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.is_me = is_me;
+        this.eps = 0.1;
+    }
+
+    start(){
+        if (this.is_me) {
+            this.add_listening_events();
+        }
+    }
+
+    add_listening_events(){
+        let outer = this;
+        this.playground.game_map.$canvas.on("contextmenu", function(){
+            return false;
+        });
+        this.playground.game_map.$canvas.mousedown(function(e) {
+            if (e.which === 3) {
+                outer.move_to(e.clientX, e.clientY);
+            }
+        });
+    }
+
+    move_to(tx,ty) {
+        console.log("move to", tx, ty);
+    }
+
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
     }
 }
 class AcGamePlayground{
@@ -130,6 +187,8 @@ class AcGamePlayground{
         this.width = this.$playground.width();
         this.height = this.$playground.height();
         this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width/2, this.height/2, this.height * 0.05, "white", this.height * 0.15, true));
     }
 
     start(){
---
+++ game/static/js/src/playground/player/zbase.js
@@ -0,0 +1,51 @@
+class Player extends AcGameObject{
+    constructor(playground, x, y, radius, color, speed, is_me){
+        super();
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.is_me = is_me;
+        this.eps = 0.1;
+    }
+
+    start(){
+        if (this.is_me) {
+            this.add_listening_events();
+        }
+    }
+
+    add_listening_events(){
+        let outer = this;
+        this.playground.game_map.$canvas.on("contextmenu", function(){
+            return false;
+        });
+        this.playground.game_map.$canvas.mousedown(function(e) {
+            if (e.which === 3) {
+                outer.move_to(e.clientX, e.clientY);
+            }
+        });
+    }
+
+    move_to(tx,ty) {
+        console.log("move to", tx, ty);
+    }
+
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+}
